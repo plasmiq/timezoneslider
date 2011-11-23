4ace0af875b7e9f88287b2127cef71f1b0806584
Krzysztof Modras <slain@yesod.(none)>
2011-11-23 10:27:23 +0100

    bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timezoneslider::Application.config.secret_token = '3d2bcb3107aba5cea7d70e09c4d3e4bc5c3e5ef07675d1014e2382639c66e0b5fb2c27455d53553c57d1d636753e45ce948b9c1ef52dc7e7e7832dd09cf60f0c'
