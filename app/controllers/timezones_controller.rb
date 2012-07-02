8cb2cc088f2edc5455b360c979e0989f772184c1
Krzysztof Modras <krzysztof.modras@gmail.com>
2012-07-02 21:40:16 +0200

    cleanups

--- app/controllers/timezones_controller.rb
+++ app/controllers/timezones_controller.rb
@@ -2,8 +2,7 @@ class TimezonesController < ApplicationController
   def search
     @locations = Timezone.search params[:term]
     respond_to do |format|
-        format.html
-        format.js
+        format.js { render :json => @locations.to_json }
     end
   end
 end
