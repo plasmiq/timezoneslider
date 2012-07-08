adfa76d15dcfd7272b371cc45fb32c0ae5b73cc6
Krzysztof Modras <krzysztof.modras@gmail.com>
2012-07-08 17:45:19 +0200

    cleanups

--- app/assets/javascripts/sliders.js
+++ app/assets/javascripts/sliders.js
@@ -1,40 +1,5 @@
 Sliders = Ember.Application.create();
 
-Sliders.SlidersController = Ember.ArrayProxy.create({
-  content: [],
-  newSliderName: null,
-  newSliderTimezone: null,
-  newSliderLocation: null,
-
-  createSlider: function() {
-    var slider = Sliders.Slider.create({
-      name: this.get("newSliderName"),
-      timezone: this.get("newSliderTimezone"),
-      location: this.get("newSliderLocation")
-    });
-
-    this.pushObject(slider);
-    
-    this.set("newSliderName",null);
-    this.set("newSliderTimezone",null);
-    this.set("newSliderLocation",null);
-  },
-  removeSlider: function(slider) {
-
-     this.forEach(function(item) {
-      if(item.get("id") === slider.get("id")) {
-        this.popObject(item);
-      }
-    }, this);
-  }
-  
-});
-
-Sliders.SlidersView = Ember.CollectionView.extend({
-  contentBinding: "Sliders.SlidersController",
-});
-
 $(function() {
-  Sliders.Slider.resourceName = 'sliders';
   Sliders.TimerController.startTicking();
 })
