c703feeba7c8bd59e40cbf0ed07d84845765dfdc
Krzysztof Modras <krzysztof.modras@gmail.com>
2012-09-22 18:18:04 +0200

    tzinfo to get proper daylight savings info

--- app/models/timezone.rb
+++ app/models/timezone.rb
@@ -1,8 +1,11 @@
 class Timezone  
   def Timezone.search term
     ret = []
-    ActiveSupport::TimeZone.all.each do |tz|
-      ret << { label: tz.name, value: tz.utc_offset } if tz.name.downcase.include? term.downcase
+    TZInfo::Timezone.all.map do |tz|
+      ret << { 
+      	label: tz.friendly_identifier(true), 
+      	value: tz.current_period.utc_total_offset
+      } if tz.name.downcase.include? term.downcase
     end
     ret
   end
