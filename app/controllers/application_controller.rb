cfe170b87323ed51a5662f8db28dd90c78a62f78
Krzysztof Modras <krzysztof.modras@gmail.com>
2012-09-26 02:15:45 +0200

    remove auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  USER, PASSWORD = 'timezoneslider', 'gen001'
- 
-  before_filter :authentication_check
- 
-  private
-    def authentication_check
-      authenticate_or_request_with_http_basic do |user, password|
-        user == USER && password == PASSWORD
-      end
-    end
 end
