04dbf213c10939d2a54fe51121f1c8c0ba9b8213
Krzysztof Modras <krzysztof.modras@gmail.com>
2012-10-01 13:25:24 +0200

    localtion name should be capitalized

--- app/assets/javascripts/sliders.js
+++ app/assets/javascripts/sliders.js
@@ -3,5 +3,5 @@ Sliders = Ember.Application.create();
 $(function() {
   var current_timezone = (new Date()).getTimezoneOffset() * (-60)
   Sliders.TimeController.startTicking();
-  Sliders.SlidersController.createSlider("ME","CURRENT LOCATION",current_timezone);
+  Sliders.SlidersController.createSlider("ME","current location",current_timezone);
 })
