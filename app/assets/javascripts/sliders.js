bd40ed4068ec4d9196cbddbd1291c81306117f8b
Krzysztof Modras <krzysztof.modras@gmail.com>
2012-10-01 12:28:24 +0200

    default slider

--- app/assets/javascripts/sliders.js
+++ app/assets/javascripts/sliders.js
@@ -1,5 +1,7 @@
 Sliders = Ember.Application.create();
 
 $(function() {
+  var current_timezone = (new Date()).getTimezoneOffset() * (-60)
   Sliders.TimeController.startTicking();
+  Sliders.SlidersController.createSlider("ME","CURRENT LOCATION",current_timezone);
 })
